15d7754ad15bea2223b28dd09bd9c52bfc9b2eae
WHYoung <williamharoldyoung@gmail.com>
2016-01-28 10:58:48 +0000

    Priority Coloring

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,6 +1,20 @@
 module AssignmentsHelper
     def assignment_list(a)
-        a_color = '" class="list-group-item list-group-item-info">'
+        case a.level
+        when 1
+            a_level = "success"
+        when 2
+            a_level = "info"
+        when 3
+            a_level = "info"
+        when 4
+            a_level = "warning"
+        when 5
+            a_level = "danger"
+        else
+            a_level = "info"
+        end
+        a_color = '" class="list-group-item list-group-item-' + a_level + '">'
 #        a_msgs = '<span class="badge">0</span>'
         ('<a href="' + assignment_path(a) + a_color + a.subject + '</a>').html_safe 
     end    
